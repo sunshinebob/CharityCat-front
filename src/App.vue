e0a297f374a98f27c1d821e5b002647468786059
sunshinebob <360444887@qq.com>
2018-07-17 11:24:21 +0800

    项目框架搭建

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    created() {
+      const year = 2016
+      const month = 2
+      const days = new Date(year, month, 0).getDate()
+      console.log(days)
+    }
+  }
+</script>
+
+<style scoped lang="sass" rel="stylesheet/sass">
+
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import Vuex from 'vuex'
+import state from './state'
+import mutations from './mutations'
+import * as getters from './getters'
+import * as actions from './actions'
+import createLogger from 'vuex/dist/logger'
+
+Vue.use(Vuex)
+
+const debug = process.env.NODE_ENV !== 'production'
+
+export default new Vuex.Store({
+  actions,
+  getters,
+  state,
+  mutations,
+  strict: debug,
+  plugins: debug ? [createLogger()] : []
+})
